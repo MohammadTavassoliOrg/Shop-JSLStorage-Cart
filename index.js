887188e9962a51007585440b23b1b7b009dbb25e
mohammadtavasoli <mohammadtavasoliprogramming@gmail.com>
2022-01-27 10:54:25 +0330

    nothing

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 let port = process.env.PORT;
 let host = process.env.HOST;
 console.log(port);
-mongoose.connect(`mongodb://${host}/27017`)
+mongoose.connect(`mongodb://${host}/shop`)
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
 
