69bd1d57a782060e2464cd23bb28ba97477e6a88
mohammadtavasoli <mohammadtavasoliprogramming@gmail.com>
2022-01-27 10:52:39 +0330

    added delete and put

--- goods.js
+++ goods.js
@@ -19,7 +19,26 @@ router.post('/', async (req, res) => {
   });
   await newgoods.save(); 
   res.status(200).send(newgoods);
-  console.log(req.body._id);
+});
+
+router.delete("/:id", async (req, res) => {
+  const removeGoods = await Goods.findByIdAndRemove(req.params.id);
+
+  if (!removeGoods) return res.status(404).send('This goods with the given ID was not found.');
+
+  res.send(removeGoods);
+});
+router.put("/:id", async (req, res) => {
+  const { error } = validateGoods(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const putGoods = await Goods.findByIdAndUpdate(req.params.id, { name: req.body.name, price:req.body.price }, {
+    new: true
+  });
+
+  if (!putGoods) return res.status(404).send('The goods with the given ID was not found.');
+  
+  res.send(putGoods);
 });
 
 module.exports = router;
