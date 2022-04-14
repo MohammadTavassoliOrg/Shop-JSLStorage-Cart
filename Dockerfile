f7cd3d749e4ceb52bae8448d4a139c912f518939
mohammadtavasoli <mohammadtavasoliprogramming@gmail.com>
2022-04-14 11:29:06 +0430

    change mainHome.js

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM node
+COPY forc /
+RUN npm install
--- axios/mainHome.js
+++ axios/mainHome.js
@@ -21,11 +21,21 @@ axios.get('http://localhost:3000/api/goods/61f68f30f11f60654da4e1e8')
       location.reload();
   });
   document.getElementById("Add-Product2").addEventListener("click", function () {
-      window.localStorage.setItem(response.data[1].name, response.data[1].price + " Toman");
+      let local = localStorage.getItem(response.data[1].name, response.data[1].price + " Toman");
+      if (local === null) {
+        window.localStorage.setItem(response.data[1].name, response.data[1].price + " Toman");
+      } else {
+        alert("You have already added this product to your cart");
+      }
       location.reload();
   });
   document.getElementById("Remove-Product2").addEventListener("click", function () {
-      localStorage.removeItem(response.data[1].name, response.data[1].price + " Toman");
+      let local = localStorage.getItem(response.data[1].name, response.data[1].price + " Toman");
+      if (local === null)  {
+        alert("This product is not available in your shopping cart")
+      } else {
+        localStorage.removeItem(response.data[1].name, response.data[1].price + " Toman");
+      }
       location.reload();
   });
   document.getElementById("Add-Product3").addEventListener("click", function () {
